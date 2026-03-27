fbcc7bc6955e1d8f5fd853b331568e26f22693f6
Kane <Kane@kokomo247.com>
2026-03-27 10:28:36 +0900

    allow access only for my website

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://yeschan119.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
